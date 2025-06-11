5d3abad1396d3da90972daf48226effbfd0d0117
Vini28k <vineela.28k@gmail.com>
2025-06-11 20:06:22 +0530

    Updated ActionButton.tsx

--- src/components/common/ActionButton.tsx
+++ src/components/common/ActionButton.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { LucideIcon } from 'lucide-react';
+import { DivideIcon as LucideIcon } from 'lucide-react';
 import type { BaseComponentProps } from '@/types';
 
 interface ActionButtonProps extends BaseComponentProps {
